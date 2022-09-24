b3b4dfb16a774d84e0d34addf3bc354f1189e142
Muhammad Azka <hello.mazka@gmail.com>
2022-09-25 01:36:08 +0700

    added some codes

--- pages/form/index.tsx
+++ pages/form/index.tsx
@@ -1,49 +1,36 @@
 import { useMutation } from "@apollo/client";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { NextPage } from "next/types";
 import { useContext, useEffect } from "react";
+import { FaCheck } from "react-icons/fa";
 import { PhonebookContext } from "../../components/context/PhonebookProvider";
 import AddContactWithPhones from "../../components/graphql/queries/addContactWithPhones";
 import ContactForm from "../../components/reusables/contactForm";
 import Container from "../../components/reusables/container";
-import DoneContactButton from "../../components/reusables/doneContactButton";
+import { DoneButtonStyled } from "../../components/reusables/doneContactButton/index.style";
 
 const Form: NextPage = () => {
+  const router = useRouter();
   const phonebookContext = useContext(PhonebookContext);
-  const [addContactWithPhones, { data, loading, error }] = useMutation(AddContactWithPhones)
+  const [addContactWithPhones, { data, loading, error, }] = useMutation(AddContactWithPhones)
 
   const handleDone = () => {
-    if (phonebookContext.firstName.length > 0 &&
-       phonebookContext.lastName.length > 0 &&
-       phonebookContext.phones.length > 0) {
-      phonebookContext.setContact([{
-        first_name: phonebookContext.firstName,
-        last_name: phonebookContext.firstName,
-        phones: phonebookContext.phones
-      }])
-    }
-  }
+    addContactWithPhones({
+      variables: {
+        first_name: phonebookContext.contact[0].first_name,
+        last_name: phonebookContext.contact[0].last_name,
+        phones: phonebookContext.contact[0].phones,
+      }
+    });
+  };
 
-  useEffect(() => {
-    if (phonebookContext.contact.length > 0) {
-      addContactWithPhones({
-        variables: {
-          first_name: phonebookContext.contact[0].first_name,
-          last_name: phonebookContext.contact[0].last_name,
-          phones: phonebookContext.contact[0].phones,
-        }
-      })
-    }
-  }, [addContactWithPhones, phonebookContext.contact, phonebookContext.contact.length])
-  
   return (
     <Container>
       {(phonebookContext.firstName.length > 0 &&
       phonebookContext.lastName.length > 0 &&
       phonebookContext.phones.length > 0) && (
-        <Link href="/" onClick={() => handleDone()}>
-          <a><DoneContactButton /></a>
-        </Link>
+        <DoneButtonStyled onClick={() => handleDone()}><FaCheck /></DoneButtonStyled>
       )}
       <ContactForm first_name={phonebookContext.firstName} last_name={phonebookContext.lastName} id={0} created_at={""} phones={[]} favorite={false} />
     </Container>
