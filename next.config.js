abd739bedf38f2e6f080455485ee86a102d1befb
Muhammad Azka <hello.mazka@gmail.com>
2022-09-25 23:49:39 +0700

    added some codes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-  reactStrictMode: true,
-  target: 'serverless',
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
 });
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+});
+module.exports = nextConfig;
